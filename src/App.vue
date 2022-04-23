b69eb8cd9c6d632928cacf0267f6a1df9ca2afe9
lv <lvjiajia@fofund.com.cn>
2022-04-23 17:05:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="item in users" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  name: 'App',
+  data() {
+    return {
+      users: null
+    }
+  },
+  async mounted() {
+    const res = await axios.get("http://localhost:3000/api/users")
+    this.users = res.data
+  }
+}
+</script>
